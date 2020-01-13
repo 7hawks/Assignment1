76cc10b409c6e37de55597dbec2c8613a4576fa8
Caleb <caleb7hawks@gmail.com>
2020-01-13 09:49:05 -0800

    added Gift and User classes to SecretSanta.Business

---
+++ SecretSanta/src/SecretSanta.Business/Gift.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Business
+{
+    class Gift
+    {
+        public int Id { get; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public User User { get; set; }
+    }
+}
