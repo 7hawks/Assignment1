e6f3eea4a509846da48bb114c62613b9c03202a9
Caleb <caleb7hawks@gmail.com>
2020-01-13 23:52:46 -0800

    done for now

--- SecretSanta/src/SecretSanta.Business/User.cs
+++ SecretSanta/src/SecretSanta.Business/User.cs
@@ -32,15 +32,16 @@ namespace SecretSanta.Business
             set => _LastName = AssertIsNotNullOrWhitespace(value);
         }
 
-        private string AssertIsNotNullOrWhitespace(string value) =>
-    value switch
-    {
-        null => throw new ArgumentNullException(nameof(value)),
-        "" => throw new ArgumentException($"{nameof(value)} cannot be an empty string.", nameof(value)),
-        string temp when string.IsNullOrWhiteSpace(temp) =>
-            throw new ArgumentException($"{nameof(value)} cannot be only whitespace.", nameof(value)),
-        _ => value
-    };
-
+        private string AssertIsNotNullOrWhitespace(string value)
+        {
+            return value switch
+            {
+                null => throw new ArgumentNullException(nameof(value)),
+                "" => throw new ArgumentException($"{nameof(value)} cannot be an empty string.", nameof(value)),
+                string temp when string.IsNullOrWhiteSpace(temp) =>
+                    throw new ArgumentException($"{nameof(value)} cannot be only whitespace.", nameof(value)),
+                _ => value
+            };
+        }
     }
 }
